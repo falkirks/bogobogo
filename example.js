58386d045194673a020b975a362b7e4bf56c102b
Falkirks <bpnoreply@gmail.com>
2015-03-23 22:49:02 -0700

    Version 0.0.1

--- example.js
+++ example.js
@@ -3,7 +3,6 @@ var bogobogo = require("./index");
 var a = [1, 2, 3, 4, 5, 6, 7];
 bogobogo.shuffle(a, 0, a.length-1);
 console.log(a);
-//console.log(bogobogo.shuffle([1,3,4,5,5,3], 0, 3));
 
 bogobogo.getBogoBogo(a).then(function(array) {
 	console.log(array);
