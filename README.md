9cce3cc48c25e4ebce485a0e4501a007ee24a749
Falkirks <bpnoreply@gmail.com>
2015-03-23 22:46:05 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+bogobogo
+========
+
+> Simple implementation of Bogobogosort
+
+
+## Install
+
+```sh
+$ npm install --save bogobogo
+```
+
+
+## Usage
+
+```js
+var bogobogo = require('bogobogo');
+
+bogobogo.create([66, 22, 3, 5, 1, 19]).then(function(result){
+	console.log(result);
+}).start();
+```
+
+
+## License
+
+MIT © [Falkirks](falkirks.com)
+
---
+++ index.js
@@ -0,0 +1,60 @@
+'use strict';
+var SortInstance = function(bogo, array){
+	this.bogo = bogo;
+	this.array = array;
+	this.cb = null;
+	this.iterations = 0;
+};
+SortInstance.prototype.start = function() {
+	process.nextTick((function(){
+        for (var n = 1; n < this.array.length; n++) {
+        	console.log(n);
+        	this.bogo.shuffle(this.array, 0, n);
+        	if(!this.bogo.isSorted(this.array, 0, n)){
+        		n = 0;
+        	}
+	    	/*
+	    	if (!this.bogo.isSorted(this.array, 0, this.array.length)){ 
+	    		n = 0; 
+	    	}*/
+		}
+		if(this.cb != null){
+		 	this.cb(this.array);
+		}
+    }).bind(this));
+    return this;
+};
+SortInstance.prototype.then = function(cb){
+	this.cb = cb;
+	return this;
+};
+
+var BogoBogo = function(){
+};
+BogoBogo.prototype.random = function(min, max) {
+	return Math.floor(min+(max-min)*Math.random());
+};
+BogoBogo.prototype.shuffle = function(array, start, end) {
+    for(var i = start; i <= end; i++){
+    	var rand = this.random(start, end);
+		var tmp = array[i];
+        array[i] = array[rand];
+        array[rand] = tmp;
+    }
+    return array;
+};
+BogoBogo.prototype.isSorted = function(array, start, end) {
+    var last = array[start];
+    for(var i = start; i <= end; i++){
+		if(last > array[i]){
+			return false;
+		}
+		last = array[i];
+    }
+    return true;
+};
+BogoBogo.prototype.create = function(array) {
+	return new SortInstance(this, array);
+};
+
+module.exports = new BogoBogo();
